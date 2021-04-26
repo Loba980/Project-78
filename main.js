348f8f7425520d055caba0f1dfddae635de14c12
Loba980 <78515908+Loba980@users.noreply.github.com>
2021-04-26 18:26:28 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+family_memeber_image=[];
+family_memeber_name=[];
+function show_family_member(){
+    family_memeber_name=["Dad", "Mom", "Sola", "Me (Loba)"];
+    family_memeber_image=["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.ca%2FFamily-Book-Todd-Parr%2Fdp%2F0316070408&psig=AOvVaw0NwxpjhOp9XIq7rKRPGC1d&ust=1619308259021000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLi2-77HlfACFQAAAAAdAAAAABAD", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F565694403175767909%2F&psig=AOvVaw2xGpfb-jyq_vhYAb7I-Kd0&ust=1619308316253000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPix59rHlfACFQAAAAAdAAAAABAY", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fcute-cartoon-a-young-mother-vector-4414509&psig=AOvVaw1FOqDk7bWiPPToNhIuL83j&ust=1619308394860000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKj4_4DIlfACFQAAAAAdAAAAABAP", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fcute-boy-cartoon-vector-16635405&psig=AOvVaw1Vgsg_L5-jcQBrDF1T6Qu6&ust=1619308427247000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOCEqpDIlfACFQAAAAAdAAAAABAI", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fgirl%2Bcartoon&psig=AOvVaw1Nk1U3xhOsCm7gH48woj5O&ust=1619308454925000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMjL7pzIlfACFQAAAAAdAAAAABAD"];
+    function show_family_memebers(){
+        
+    }
+}
